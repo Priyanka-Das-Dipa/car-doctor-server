2ec5f51e6614e9cee211cc1d1c721da4b9228a58
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-10-27 16:11:41 +0600

    booking db added

--- index.js
+++ index.js
@@ -42,14 +42,28 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
-        projection: { title: 1, img: 1, price: 1, service_id: 1 },
+        projection: { title: 1, img: 1, price: 1, service_id: 1 , img: 1},
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
 
+    // booking 
+    app.get('/bookings', async(req, res)=>{
+      console.log(req.query.email)
+      let query = {}
+      if(req.query?.email){
+        query = {email : req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
     app.post('/bookings', async(req, res) =>{
         const bookings = req.body;
+        console.log(bookings)
+        const result = await bookingCollection.insertOne(bookings)
+        res.send(result)
     })
 
     // Send a ping to confirm a successful connection
